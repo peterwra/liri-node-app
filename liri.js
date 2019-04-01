f01384c25012eace7ea50f4595ac4abde5f172e2
Peter W <42481836+peterwra@users.noreply.github.com>
2019-04-01 00:09:09 -0500

    concert-this added

--- liri.js
+++ liri.js
@@ -5,23 +5,27 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 // Create spotify variable
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 // File system variable for file I/O
 var fs = require("fs");
 
+// Axios variable
+var axios = require("axios");
+
 // Get user choice and search item
-var userChoice = process.env[2];
+var userChoice = process.argv[2].toLowerCase();
 var searchItem = "";
-for (var i = 3; i < process.env.length; i++){
-    searchItem+=process.env[i]+((i+1)==process.env.length ? "" : " ");
+for (var i = 3; i < process.argv.length; i++) {
+    searchItem += process.argv[i] + ((i + 1) == process.argv.length ? "" : " ");
 }
+console.log("'" + searchItem + "'");
 
 // Call function based on user choice
 switch (userChoice) {
     case "help":
         console.log("The following options are valid:");
-        console.log("----------------------------------------------------------------------------------");
+        console.log("-----------------------------------------------------------------------");
         console.log("'node liri concert-this <artist>' --> search bandsintown for the artist");
         console.log("'node liri spotify-this-song <song>' --> search spotify for the song");
         console.log("'node liri movie-this <movie>' --> search omdb for the movie");
@@ -40,6 +44,28 @@ switch (userChoice) {
         doWhatItSays(searchItem);
         break;
     default:
-        console.log("You gave me '" + userChoice + "' which is not a valid option. Enter 'liri help' to display valid options.");
+        console.log("You gave me '" + userChoice + "' which is not a valid option. Enter 'node liri help' to display valid options.");
 }
 
+function doConcertThis(artist) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl)
+        .then(function (response) {
+            var data = response.data;
+            if (data=="\n{warn=Not found}\n") {
+                console.log("We're sorry, artist '" + artist + "' doesn't seem to exist.");
+            } else if (data.length==0) {
+                console.log("We're sorry, '" + artist + "' does not have any planned concerts right now.")
+            } else {
+                for (var i = 0; i < data.length; i++) {
+                    console.log("------------------------------------");
+                    console.log("Venue Name: " + data[i].venue.name);
+                    console.log("Venue Location: " + data[i].venue.city + ", " + data[i].venue.country);
+                    console.log("Event Date: " + data[i].datetime);
+                }
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
