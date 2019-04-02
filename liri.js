c4514f9091bbbdda154869ae9d1e1e07dcb6891b
Peter W <42481836+peterwra@users.noreply.github.com>
2019-04-02 00:04:07 -0500

    Spotify search functionality

--- liri.js
+++ liri.js
@@ -4,14 +4,17 @@ require("dotenv").config();
 // Get spotify information and store in object
 var keys = require("./keys.js");
 
-// Create spotify variable
-// var spotify = new Spotify(keys.spotify);
+// Spotify variable
+var Spotify = require("node-spotify-api");
 
-// File system variable for file I/O
-var fs = require("fs");
+// Create spotify variable object
+var spotifyObj = new Spotify(keys.spotify);
 
 // Axios variable
 var axios = require("axios");
+
+// File system variable for file I/O
+var fs = require("fs");
 
 // Get user choice and search item
 var userChoice = process.argv[2].toLowerCase();
@@ -47,16 +50,20 @@ switch (userChoice) {
         console.log("You gave me '" + userChoice + "' which is not a valid option. Enter 'node liri help' to display valid options.");
 }
 
+// Look up the concert information for the artist
 function doConcertThis(artist) {
     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(queryUrl)
         .then(function (response) {
             var data = response.data;
-            if (data=="\n{warn=Not found}\n") {
+            if (data == "\n{warn=Not found}\n") {
+                // Artist not found
                 console.log("We're sorry, artist '" + artist + "' doesn't seem to exist.");
-            } else if (data.length==0) {
+            } else if (data.length == 0) {
+                // No planned concerts
                 console.log("We're sorry, '" + artist + "' does not have any planned concerts right now.")
             } else {
+                // Print concert information
                 for (var i = 0; i < data.length; i++) {
                     console.log("------------------------------------");
                     console.log("Venue Name: " + data[i].venue.name);
@@ -68,4 +75,31 @@ function doConcertThis(artist) {
         .catch(function (error) {
             console.log(error);
         });
+}
+
+// Use Spotify to look up song information
+function doSpotifyThisSong(song) {
+
+    // If no song provided, default to 'The Sign' by Ace of Base
+    var querySong = (song == "" ? "The Sign" : song)
+
+    // Perform search
+    spotifyObj.search({ type: 'track', query: querySong }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred with node-spotify-api: ' + err);
+        }
+        var response = data.tracks.items;
+        // Loop through and print the information
+        for (var i = 0; i < response.length; i++) {
+            var artist = response[i].artists[0].name;
+            var songName = response[i].name;
+            var previewLink = response[i].album.external_urls.spotify;
+            var album = response[i].album.name;
+            console.log("------------------------------------");
+            console.log("Artist: " + artist);
+            console.log("Song Name: " + songName);
+            console.log("Spotify Preview Link: " + previewLink);
+            console.log("Album: " + album);
+        }
+    });
 }
